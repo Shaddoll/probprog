a87e1559b9473e00597348171c7383eade406ba8
El Shaddai <chenzijian13@gmail.com>
2018-10-22 19:40:55 -0400

    finish klqp

--- main.py
+++ main.py
@@ -4,41 +4,41 @@ from models.lda import LDA
 import tensorflow as tf
 
 
-datafile = "DataPreprocess/nipstxt/nipstoy/doc_wordID_short*.txt"
+datafile = "DataPreprocess/nipstxt/nipstoy20/doc_wordID_short*.txt"
 txt_files = glob.glob(datafile)
 D = len(txt_files)  # number of documents
 print("number of documents, D: {}".format(D))
 N = [0] * D  # words per doc
 K = 5  # number of topics
-T = 30
-S = 200
+T = 2000
+S = 10
 wordIds = [None] * D
 count = 0  # count number of documents
 for file in (txt_files):
     with open(file, 'rt', encoding="ISO-8859-1") as f:
         wordIds[count] = list(map(int, f.readline().split()))
         N[count] = len(wordIds[count])
-        wordIds[count] = np.array(wordIds[count])
+        wordIds[count] = np.array(wordIds[count]).astype('int32')
     print("load" + file + "finished")
     count += 1
 IdtoWord = {}
 vocab = set()
-with open("DataPreprocess/wordToIDtoy.txt") as f:
+with open("DataPreprocess/wordToIDtoy20.txt") as f:
     for line in f:
         line = line.split()
         IdtoWord[int(line[1])] = line[0]
         vocab.add(line[0])
 V = len(vocab)  # vocabulary size21
+tokens = [None] * V
+for key in IdtoWord:
+    tokens[key] = IdtoWord[key]
 print("vocab size is {}".format(V))
 print("load wordToIDtoy.txt finished")
 
 model = LDA(K, V, D, N)
 print("model constructed")
-#model.gibbs(wordIds, S, T)
-model.klqp(wordIds, T)
-tokens = [None] * V
-for key in IdtoWord:
-    tokens[key] = IdtoWord[key]
+model.gibbs(wordIds, S, T)
+#model.klqp(wordIds, S, T)
 model.criticize(tokens)
 
 def assert_close(
--- models/lda.py
+++ models/lda.py
@@ -6,33 +6,35 @@ from edward.models import Dirichlet, ParamMixture, Categorical, Empirical
 
 class LDA(object):
     def __init__(self, K, V, D, N):
-        self.K = K
-        self.V = V
-        self.D = D
-        self.N = N
+        self.K = K # number of topics
+        self.V = V # vocabulary size
+        self.D = D # number of documents
+        self.N = N # number of words of each document
         alpha = tf.zeros([K]) + 0.1
-        yita = tf.zeros([V]) + 0.05
+        yita = tf.zeros([V]) + 0.01
         self.theta = [None] * D
         self.beta = Dirichlet(yita, sample_shape=K)
-        #self.beta = Dirichlet(yita)
         self.z = [None] * D
         self.w = [None] * D
         for d in range(D):
             self.theta[d] = Dirichlet(alpha)
             self.w[d] = ParamMixture(mixing_weights=self.theta[d],
-                                     #component_params={'probs': self.beta},
-                                     component_params={'logits': (self.beta)},
+                                     component_params={'probs': self.beta + 1.e-36},
                                      component_dist=Categorical,
                                      sample_shape=N[d],
-                                     validate_args=True)
+                                     validate_args=False)
             self.z[d] = self.w[d].cat
 
-    def __run_inference__(self, T):
-        self.inference.initialize(n_iter=T, n_print=10, logdir='log', n_samples=100)
+    def __run_inference__(self, T, S=None):
+        #self.inference.initialize(n_iter=T, n_print=1, n_samples=S, optimizer='adam')
+        self.inference.initialize(n_iter=T, n_print=1)
         tf.global_variables_initializer().run()
         for n in range(self.inference.n_iter):
             info_dict = self.inference.update()
             self.inference.print_progress(info_dict)
+            #print('\nit: {}, qz_var_est: {}'.format(n+1, self.qz_var[0].eval()))
+            #print('\nit: {}, qtheta_var_est: {}'.format(n+1, self.qtheta_var[0].eval()))
+            #print('\nit: {}, qbeta_var_est:\n {}'.format(n+1, self.qbeta_var.eval()))
         self.inference.finalize()
 
     def gibbs(self, wordIds, S, T):
@@ -42,7 +44,7 @@ class LDA(object):
         N = self.N
         latent_vars = {}
         training_data = {}
-        qbeta = Empirical(tf.Variable(tf.zeros([S, K, V])))
+        qbeta = Empirical(tf.Variable(tf.ones([S, K, V]) / V))
         latent_vars[self.beta] = qbeta
         qtheta = [None] * D
         qz = [None] * D
@@ -57,32 +59,38 @@ class LDA(object):
         print("gibbs setup finished")
         self.__run_inference__(T)
 
-    def klqp(self, wordIds, T):
+    def klqp(self, wordIds, S, T):
         K = self.K
         V = self.V
         D = self.D
         N = self.N
         latent_vars = {}
         training_data = {}
-        qbeta = Dirichlet(tf.nn.softplus(tf.Variable(tf.zeros([K, V])+0.05)))
+        self.qbeta_var = tf.nn.softplus(tf.Variable(tf.ones([K, V]) / V))
+        #self.qbeta_var = tf.nn.softplus(tf.get_variable("qbeta/conc", [K, V]))
+        qbeta = Dirichlet(self.qbeta_var)
         latent_vars[self.beta] = qbeta
         qtheta = [None] * D
         qz = [None] * D
+        self.qtheta_var = [tf.nn.softplus(tf.Variable(tf.ones([K]) / K)) for d in range(D)]
+        #self.qtheta_var = [tf.nn.softplus(tf.get_variable("qtheta%d"%d, [K])) for d in range(D)]
+        self.qz_var = [tf.nn.softmax(tf.Variable(tf.ones([K]) / K)) for d in range(D)]
+        #self.qz_var = [tf.nn.softmax(tf.get_variable("qz%d"%d, [K])) for d in range(D)]
         for d in range(D):
-            qtheta[d] = Dirichlet(tf.nn.softplus(tf.Variable(tf.zeros([K])+0.1)))
+            qtheta[d] = Dirichlet(self.qtheta_var[d])
             latent_vars[self.theta[d]] = qtheta[d]
-            qz[d] = Categorical((tf.nn.softmax(tf.Variable(tf.zeros([K]) + 0.1))), dtype=tf.int32, sample_shape=[N[d]])
+            qz[d] = Categorical(probs=self.qz_var[d], dtype=tf.int32, sample_shape=[N[d]])
             latent_vars[self.z[d]] = qz[d]
             training_data[self.w[d]] = wordIds[d]
         self.latent_vars = latent_vars
         self.inference = ed.KLqp(latent_vars, data=training_data)
+        #self.inference = ed.ReparameterizationKLqp(latent_vars, data=training_data)
         print("klqp setup finished")
-        self.__run_inference__(T)
+        self.__run_inference__(T, S)
 
 
     def criticize(self, tokens):
         K = self.K
-        #qbeta_sample = self.latent_vars[self.beta].params[-1].eval()
         qbeta_sample = self.latent_vars[self.beta].sample().eval()
         pickle.dump(qbeta_sample, open("train2.p", "wb"))
         prob = [None] * K
