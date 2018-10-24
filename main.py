8feeab765b408e5965e7028bfa7793849c4c86a8
El Shaddai <chenzijian13@gmail.com>
2018-10-23 21:55:51 -0400

    ..

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import glob
 from models.lda import LDA
 import tensorflow as tf
 
-
+"""
 datafile = "DataPreprocess/nipstxt/nipstoy20/doc_wordID_short*.txt"
 txt_files = glob.glob(datafile)
 D = len(txt_files)  # number of documents
 print("number of documents, D: {}".format(D))
 N = [0] * D  # words per doc
 K = 5  # number of topics
-T = 2000
+T = 100
 S = 10
 wordIds = [None] * D
 count = 0  # count number of documents
@@ -34,29 +34,19 @@ for key in IdtoWord:
     tokens[key] = IdtoWord[key]
 print("vocab size is {}".format(V))
 print("load wordToIDtoy.txt finished")
+"""
+import pickle
+wordIds = pickle.load(open("toy.dat", "rb"))
+tokens = list(pickle.load(open("vocab.dat", "rb")))
+K = 5
+V = len(tokens)
+D = len(wordIds)
+N = [len(wordIds[d]) for d in range(D)]
+S = 10
+T = 100
 
 model = LDA(K, V, D, N)
 print("model constructed")
 model.gibbs(wordIds, S, T)
 #model.klqp(wordIds, S, T)
 model.criticize(tokens)
-
-def assert_close(
-    x, y, data=None, summarize=None, message=None, name="assert_close"):
-    message = message or ""
-    x = tf.convert_to_tensor(x, name="x")
-    y = tf.convert_to_tensor(y, name="y")
-    if data is None:
-        data = [
-            message,
-            "Condition x ~= y did not hold element-wise: x = ", x.name, x, "y = ",
-            y.name, y
-        ]
-    if x.dtype.is_integer:
-        return tf.assert_equal(
-            x, y, data=data, summarize=summarize, message=message, name=name)
-    with tf.name_scope(name, "assert_close", [x, y, data]):
-        tol = np.finfo(x.dtype.as_numpy_dtype).eps
-        condition = tf.reduce_all(tf.less_equal(tf.abs(x - y), tol))
-        return tf.Assert(
-            condition, data, summarize=summarize)
